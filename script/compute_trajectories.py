b6a5377a0d9a3dbed086bd160db4fc12b9988eca
Stefan Scherzinger <scherzin@fzi.de>
2020-11-24 13:46:52 +0100

    Provide functions for the computation of trajectories

--- script/compute_trajectories.py
+++ script/compute_trajectories.py
@@ -3,38 +3,32 @@ from rosbag import Bag
 import numpy as np
 import os
 import csv
-"""
-Compute a state-dimensional trajectory from a rosbag recording.
-The trajectory has position, velocity and acceleration values for each of the
-state's dimensions.
 
-The trajectory is saved as a .csv file in the trajectories subfolder of this
-package, keeping the rosbag's name.
-"""
 
-# Read states from rosbag and separate dimensions
-states = []
-time = []
-bagfile = '../rosbags/example.bag'
-with Bag(bagfile) as bag:
-    start = bag.get_start_time()
-    for topic, msg, t in bag:
-        if topic == '/state_output':
-            states.append([
-                msg.pose.position.x,
-                msg.pose.position.y,
-                msg.pose.position.z,
-                msg.pose.orientation.x,
-                msg.pose.orientation.y,
-                msg.pose.orientation.z,
-                msg.pose.orientation.w,
-                msg.gripper.data
-            ])
-            time.append(t.to_sec() - start)  # Seconds from start
+def read_rosbag(bagfile):
+    """ Read states from a rosbag file """
+    states = []
+    time = []
+    with Bag(bagfile) as bag:
+        start = bag.get_start_time()
+        for topic, msg, t in bag:
+            if topic == '/state_output':
+                states.append([
+                    msg.pose.position.x,
+                    msg.pose.position.y,
+                    msg.pose.position.z,
+                    msg.pose.orientation.x,
+                    msg.pose.orientation.y,
+                    msg.pose.orientation.z,
+                    msg.pose.orientation.w,
+                    msg.gripper.data
+                ])
+                time.append(t.to_sec() - start)  # Seconds from start
 
-# Make states an array of sampled functions
-# states = [x(t), y(t), z(t), qx(t), qy(t), qz(t), qw(t), g(t)]
-states = np.array(states).transpose()
+    # Make states an array of sampled functions
+    # states = [x(t), y(t), z(t), qx(t), qy(t), qz(t), qw(t), g(t)]
+    states = np.array(states).transpose()
+    return time, states
 
 
 def diff(states, h):
@@ -69,37 +63,64 @@ def diff(states, h):
     return np.array(values_dot)
 
 
-# Differentiation
-h = time[-1] / len(time)  # step width
-states_dot = diff(states, h)
-states_ddot = diff(states_dot, h)
+class Trajectory(object):
+    """ A minimal trajectory class"""
+
+    def __init__(self, times, states, states_dot, states_ddot):
+        self.times = times
+        self.states = states
+        self.states_dot = states_dot
+        self.states_ddot = states_ddot
 
 
-#--------------------------------------------------------------------------------
-# Write trajectory as .csv to the ../trajectories folder
-#--------------------------------------------------------------------------------
+def compute_trajectory(bagfile):
+    """ Compute a trajectory from a given rosbag file
 
-# Create trajectory folder if non-existent
-traj_folder = "../trajectories"
-if not os.path.exists(traj_folder):
-    os.mkdir(traj_folder)
+    The trajectory has position, velocity and acceleration values for each of the
+    state's dimensions.
+    """
 
-# Re-use recorded rosbag name for .csv file
-outfile = bagfile.split('/')[-1]
-outfile = outfile.split('.')[0] + '.csv'
-outfile = '{}/{}'.format(traj_folder, outfile)
+    times, states = read_rosbag(bagfile)
 
-# Make data time major for easier writing
-states = np.array(states).transpose()
-states_dot = np.array(states_dot).transpose()
-states_ddot = np.array(states_ddot).transpose()
+    # Differentiation
+    h = times[-1] / len(times)  # step width
+    states_dot = diff(states, h)
+    states_ddot = diff(states_dot, h)
 
-# Save values as .csv files.
-# Format: time (1 value), states (8 values), d/dt states (8 values), d^2/dt^2 states (8 values)
-with open(outfile, 'w') as f:
-    writer = csv.writer(f, delimiter=' ')
-    for i in range(len(time)):
-        writer.writerow([time[i]] + states[i].tolist() + states_dot[i].tolist() + states_ddot[i].tolist())
+    return Trajectory(times, states, states_dot, states_ddot)
 
-# Finish
-print("done")
+
+def save_trajectory(file_name, trajectory):
+    """ Write trajectory as .csv file in the trajectories folder
+    """
+    # Create trajectory folder if non-existent
+    traj_folder = "../trajectories"
+    if not os.path.exists(traj_folder):
+        os.mkdir(traj_folder)
+
+    outfile = '{}/{}'.format(traj_folder, file_name)
+
+    # Make data time major for easier writing
+    states = np.array(trajectory.states).transpose()
+    states_dot = np.array(trajectory.states_dot).transpose()
+    states_ddot = np.array(trajectory.states_ddot).transpose()
+
+    # Save values as .csv files.
+    # Format: time (1 value), states (8 values), d/dt states (8 values), d^2/dt^2 states (8 values)
+    with open(outfile, 'w') as f:
+        writer = csv.writer(f, delimiter=' ')
+        for i in range(len(trajectory.times)):
+            writer.writerow([trajectory.times[i]] + states[i].tolist() + states_dot[i].tolist() + states_ddot[i].tolist())
+
+
+if __name__ == '__main__':
+    bagfile = '../rosbags/example.bag'
+
+    # Re-use recorded rosbag name for .csv file
+    outfile = bagfile.split('/')[-1]
+    outfile = outfile.split('.')[0] + '.csv'
+
+    trajectory = compute_trajectory(bagfile)
+    save_trajectory(outfile, trajectory)
+
+    print("done")
