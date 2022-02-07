751ba61f68ca8facaf25ba259f9652bb0f07af51
Stefan Scherzinger <scherzin@fzi.de>
2022-02-07 20:20:32 +0100

    Add license files
    Also add license statement to every relevant source file.

--- LICENSES/license.py
+++ LICENSES/license.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 ################################################################################
 # Copyright 2022 FZI Research Center for Information Technology
 # 
@@ -13,19 +12,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import arne_skill_pipeline.trajectories as traj
-
-""" Read a trajectory .csv file and plot the state-dimensional time series to pdf
-"""
-
-if __name__ == '__main__':
-    traj_file = '../trajectories/example.csv'
-
-    # Drog .csv extension and save as pdf.
-    file_name = traj_file.split('/')[-1]
-    file_name = file_name.split('.')[0] + '.pdf'
-
-    trajectory = traj.read_trajectory(traj_file)
-    traj.plot_trajectory(trajectory, file_name)
-    print("done")
